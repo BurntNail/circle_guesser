ff9b11ead5837bf72fc717897e49e4b08e53d3f9
Jack Maguire <49815482+BurntNail@users.noreply.github.com>
2022-12-07 11:56:24 +0000

    make clippy stricter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![warn(clippy::all, clippy::pedantic, clippy::nursery)]
+
 use crate::app::CircleGuesserApp;
 use piston_window::{
     Button, Key, MouseButton, MouseCursorEvent, PistonWindow, PressEvent, RenderEvent,
